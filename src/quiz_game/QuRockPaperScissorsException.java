7f63c945d9237bd2ec5e714807a0630b8a5ade59
dlwns <dlwns@DESKTOP-7L7F8VJ>
2020-03-10 00:27:27 +0900

    0310

--- src/quiz_game/QuRockPaperScissorsException.java
+++
@@ -1,9 +0,0 @@
-package quiz_game;
-
-public class QuRockPaperScissorsException
-{
-	public static void main(String[] args)
-	{
-		
-	}
-}
--- src/quiz_game/QuRockPaperScissorsGame.java
+++ src/quiz_game/QuRockPaperScissorsGame.java
@@ -8,115 +8,68 @@ class WrongNumberException extends Exception
 {
 	public WrongNumberException()
 	{
-		super("[예외발생] 게임과 관련없는 숫자를 입력했습니다. 다시 입력해주세요.");
+		super("게임과 관련없는 숫자입니다.");
 	}
 }
 
 public class QuRockPaperScissorsGame
 {
 
-	static Scanner scanner = new Scanner(System.in);
-	static int gameCount = 0; // 게임의 횟수를 카운트하기 위한 변수
-
 	public static int inputNum() throws WrongNumberException
 	{
-		int inputNum = 0;
-		try
-		{
-			inputNum = scanner.nextInt();
-		} catch (InputMismatchException e)
-		{
-			System.out.println("[예외발생] 문자를 입력했습니다. 숫자를 입력해주세요.");
-		}
+		Scanner scanner = new Scanner(System.in);
+		int inputNum = scanner.nextInt();
+		System.out.println(inputNum);
 
-		// 게임과 관련없는 숫자를 입력했을떄
-		if (inputNum < 0 || inputNum > 3)
+		// 1~3 외의 수가 입력된 경우 예외발생
+		if (inputNum < 1 || inputNum > 3)
 		{
-			WrongNumberException ex = new WrongNumberException();
-			throw ex;
+			throw new WrongNumberException();
 		}
 		return inputNum;
 	}
 
-	public static int initUser()
+	public static int inputNum2() throws WrongNumberException
 	{
-		int user = 0;
-		System.out.println("가위바위보를 입력하세요");
-		System.out.print("가위(1),바위(2),보(3)=>");
-		try
+		Scanner scanner = new Scanner(System.in);
+		int inputNum = scanner.nextInt();
+
+		// 0~1 외의 수가 입력된 경우 예외발생
+		if (inputNum < 0 || inputNum > 1)
 		{
-			user = inputNum();
-		} catch (WrongNumberException e)
-		{
-			System.out.println("[예외발생] " + e.getMessage());
+			throw new WrongNumberException();
 		}
-		return user;
+		return inputNum;
 	}
 
-	public static int initCom()
+	public static void rpsGameStart()
 	{
+
 		Random random = new Random();
-		// 난수생성 - 컴퓨터입장의 가위/바위/보
-		int com = random.nextInt(10000) % 3 + 1;// 1~3사이의난수생성
-		System.out.println("컴퓨터:" + com);
-		return com;
-	}
-
-	public static int reAnswer()
-	{
-		int restart = 0;
-		System.out.print("===게임재시작(1), 종료(0)===");
-		try
-		{
-			restart = inputNum();
-		} catch (WrongNumberException e)
-		{
-			System.out.println("[예외발생] " + e.getMessage());
-		}
-		return restart;
-
-	}
-
-	public static void reGame() throws WrongNumberException
-	{
-		// 5번의 게임이 되었는지 판단후 재시작/종료 확인
-		if (gameCount >= 5)
-		{
-			System.out.println("게임을 또 하시겠습니까?");
-			int restart;
-
-			while (true)
-			{
-				restart = reAnswer();
-
-				if (restart == 0 || restart == 1)
-				{
-					// 정상입력이면 루프탈출
-					break;
-				} else
-				{
-					System.out.println("잘못입력해씸ㅜㅜ\n");
-				}
-			}
-			if (restart == 0)
-			{
-				System.exit(0);
-			} else
-			{
-				System.out.println("게임 재시작^^");
-				gameCount = 0;
-				rpsGameStart();
-			}
-		}
-	}
-
-	public static void rpsGameStart() throws WrongNumberException
-	{
-		int user = initUser();
+		int gameCount = 0;// 게임의 횟수를 카운트하기 위한 변수
 
 		while (true)
 		{
-			int com = initCom();
+
+			// 1.난수생성 - 컴퓨터입장의 가위/바위/보
+			int com = random.nextInt(10000) % 3 + 1;// 1~3사이의난수생성
+			System.out.println("컴퓨터:" + com);
+
+			// 2.사용자 입력
+			int user = 0;
+			System.out.println("가위바위보를 입력하세요");
+			System.out.print("가위(1),바위(2),보(3)=>");
+			try
+			{
+				user = inputNum();
+			} catch (WrongNumberException e)
+			{
+				System.out.println("[예외발생] " + e.getMessage());
+			} catch (InputMismatchException e)
+			{
+				System.out.println("[예외발생] 문자를 입력했습니다. 숫자를 입력하세요");
+			}
+
 			// 3.승부판단 및 결과출력
 			if (!(user < 1 || user > 3))
 			{
@@ -141,12 +94,52 @@ public class QuRockPaperScissorsGame
 
 				// 게임카운트 증가
 				gameCount++;
-				reGame();
-			} else
+			}
+
+			// 6.5번의 게임이 되었는지 판단후 재시작/종료 확인
+			if (gameCount >= 5)
 			{
-				rpsGameStart();
+
+				int restart = 0;
+				while (true)
+				{
+					Scanner scanner = new Scanner(System.in);
+					System.out.print("게임재시작(1), 종료(0):");
+					try
+					{
+						restart = inputNum2();
+
+					} catch (WrongNumberException e)
+					{
+						restart = 2;
+
+						System.out.println("[예외발생] " + e.getMessage());
+					} catch (InputMismatchException e)
+					{
+						restart = 2;
+						System.out.println("[예외발생] 문자를 입력했습니다. 숫자를 입력하세요");
+					}
+					if (restart == 0 || restart == 1)
+					{
+						// 정상입력이면 루프탈출
+						break;
+					} else
+					{
+						System.out.println("잘못입력해씸ㅜㅜ\n");
+					}
+				}
+				if (restart == 0)
+				{
+					// 게임종료를 위해 while루프 탈출
+					break;
+				} else
+				{
+					System.out.println("게임 재시작^^");
+					gameCount = 0;
+				}
 			}
 		}
+
 	}
 
 	// 가위 바위 보를 출력하는 메소드
@@ -168,7 +161,7 @@ public class QuRockPaperScissorsGame
 		return str;
 	}
 
-	public static void main(String[] args) throws WrongNumberException
+	public static void main(String[] args)
 	{
 		// 가위바위보 게임 메소드 호출
 		rpsGameStart();
