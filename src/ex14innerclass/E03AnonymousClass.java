817f8fce8d53dfbc561701c5315c7a79084c3da9
zzo2741 <dlwnstmd@gmail.com>
2020-06-29 17:44:46 +0900

    0629

--- src/ex14innerclass/E03AnonymousClass.java
+++ src/ex14innerclass/E03AnonymousClass.java
@@ -7,7 +7,7 @@ package ex14innerclass;
  * - 부모클래스의 메소드를 오버라이딩 하는것이 목적이다.
  * - 클래스를 마치 메소드를 정의하는 것과 비슷하다.
  * - 마지막에 세미콜론을 반드시 써줘야 한다.
- * 
+ *
  * 형식]
  * 		부모클래스명 참조변수 = new 부모클래스명(){
  * 			익명클래스의 실행부;
@@ -85,7 +85,6 @@ public class E03AnonymousClass
 
 		System.out.println("=============================");
 
-		
 		/*
 		 * Person클래스를 상속받은 익명클래스 정의 및 생성
 		 * : Person타입의 인스턴스 변수에 Person을 상속받은 익명클래스를 생성해서 그 주소를 할당한다.
@@ -99,6 +98,7 @@ public class E03AnonymousClass
 			 * 익명클래스 내에서만 접근 가능하다.
 			 */
 			int newVar;
+
 			void newMethod()
 			{
 				System.out.println("익명클래스에서 새롭게 확장한 메소드");
@@ -114,21 +114,22 @@ public class E03AnonymousClass
 
 			}
 		};
-		
+
 		anonyPerson.printInfo(); // 호출 가능
 		/*
 		 * 부모타입의 참조변수로 하위객체에서 확장한 멤버에 접근할 수 없으므로 위 부분은 에러가 발생한다.
 		 */
 		// anonyPerson.newMethod(); [접근불가 - 에러발생]
-		
+
 		System.out.println("=============================");
-		
+
 		//Student클래스를 상속받아 정의한 익명클래스 (즉, Student의 하위 익명객체)
-		Student annoyStudent = new Student("이준모", "2001") {
-			
+		Student annoyStudent = new Student("이준모", "2001")
+		{
+
 			// 익명클래스에서 확장한 멤버변수
 			int age = 1;
-			
+
 			@Override
 			String getInfo()
 			{
@@ -141,37 +142,35 @@ public class E03AnonymousClass
 				System.out.println("이름 : " + name + ", " + getInfo());
 			}
 		};
-		
+
 		System.out.println("[Student클래스를 상속받은 익명클래스]");
 		annoyStudent.printInfo();
-		
+
 		/*
 		 * 하위 익명클래스에서 확장한 변수이므로 익명객체를 통해서는 접근불가.
 		 */
 		//annoyStudent.age = 100; [접근불가 - 에러발생]
 		//((Student)annoyStudent).age = 100; [다운캐스팅 불가 - 에러발생]
-		
+
 		System.out.println("=============================");
 
-		
 		/*
 		 * 다운캐스팅은 *(자식틀래스명)부모타입의 참조변수* 형태로 형변환하여 하위클래스의 멤버에 접근하기 위한 수단이다.
 		 * 그러나 익명클래스는 클래스명이 없기때문에 이와 같이 다운캐스팅이 불가능하다.
 		 * 즉, 익명클래스에서는 새롭게 정의한 멤버변수를 접근할 수 없다.
 		 * 따라서 익명클래스는 오버라이딩의 목적으로만 사용해야 한다.
-		 * 
+		 *
 		Person anonyTest = new Student("다운캐스팅", "안되욤") {
 			String jumin = "951207";
 		};
 		((Student)anonyTest).jumin = "970919";
 		 */
-		 
-		
+
 		//추상클래스를 상속받은 익명클래스
 		AbstractClass ac = new AbstractClass()
 		{
 			String abStr = "확장변수";
-			
+
 			@Override
 			void abstractMethod()
 			{
@@ -180,7 +179,7 @@ public class E03AnonymousClass
 			}
 		};
 		ac.abstractMethod();
-		
+
 		System.out.println("=============================");
 
 		//인터페이스를 구현한 익명클래스
@@ -191,7 +190,7 @@ public class E03AnonymousClass
 			@Override
 			public void abstractMethod()
 			{
-				
+
 				System.out.println("인터페이스의 추상메소드 오버라이딩");
 				System.out.println("interVar = " + interVar);
 			}
